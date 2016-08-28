f997cb7f7c346a8b0b6e4def48ed0a7a25f6e8e8
mihai93 <mihailistov@gmail.com>
2016-08-28 01:38:44 -0400

    Beta 0.01

---
+++ exercise.py
@@ -0,0 +1,43 @@
+class Exercise:
+    id = 0 # global exercise id
+
+    # bare min init b/c user may add new exercises
+    def __init__(self, name):
+        self.name = name
+        Exercise.id += 1
+
+    # def set_rating(self, rating)
+    #     self.rating = rating
+
+    def set_type(self, type):
+        self.type = type
+
+    def set_muscle(self, muscle):
+        self.muscle = muscle
+
+    def set_other_muscles(self, other_muscles):
+        self.other_muscles = other_muscles
+
+    def set_equipment(self, equipment):
+        self.equipment = equipment
+
+    def set_mechanics(self, mechanics):
+        self.mechanics = mechanics
+
+    def set_level(self, level):
+        self.level = level
+
+    def set_sport(self, sport):
+        self.sport = sport
+
+    def set_force(self, force):
+        self.force = force
+
+    def set_guide_items(self, guide_items):
+        self.guide_items = guide_items
+
+    def set_note_title(self, note_title):
+        self.note_title = note_title
+
+    def set_notes(self, notes):
+        self.notes = notes
--- exercise_scrapper.py
+++ exercise_scrapper.py
@@ -1,3 +1,9 @@
+import sys
+import os
+clear = lambda: os.system('cls')
+clear()
+
+import json
 import mechanize
 from lxml import html
 import requests
@@ -5,6 +11,8 @@ import cookielib
 import urllib, urllib2
 from bs4 import BeautifulSoup
 import pprint
+import os.path
+from exercise import Exercise
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -21,18 +29,110 @@ browser.set_handle_robots(False)
 browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 browser.addheaders = [('User-agent', 'WatcherBot')]
 
-page = browser.open('http://www.bodybuilding.com/exercises/detail/view/name/pushups')
+# #############################################################################
+# The following is used for getting the url list on first run
+# No need to run twice, as it loads from urls.txt
+# #############################################################################
 
-details = BeautifulSoup(page.read(), "lxml").find(id="exerciseDetails")
+if not os.path.isfile("urls.txt"):
+    urls_out = open("urls.txt", "w")
 
-data = {}
+    alphaUrls = [];
+    urls = [];
+    listResults = [];
 
-name = details.h1.string.replace("\n","").strip()
-data['Exercise Name'] = name
+    alphabet = list(map(chr, range(97, 123)))
 
-for span in details.find_all("span"):
-    key = span.getText().split(':')[0]
-    value = span.getText().split(':')[1].replace("\n","").strip()
-    data[key] = value
+    # generate list of exercise pages in alphabetical format
+    for letter in alphabet:
+        if letter != 'x': # no exercises starting with x
+            alphaUrls.append('http://www.bodybuilding.com/exercises/list/index/selected/'+letter)
 
-pp.pprint(data)
+    # iterate through each page in the alphabet and collect exercise urls
+    for alphaUrl in alphaUrls:
+        page = browser.open(alphaUrl)
+
+        listResults = BeautifulSoup(page.read(), "lxml").find(id="listResults")
+
+        for exerciseName in listResults.find_all("div", {"class" : "exerciseName" }):
+            exerciseUrl = exerciseName.h3.a.get('href')
+            urls_out.write(exerciseUrl+"\n")
+            urls.append(exerciseUrl)
+            print exerciseUrl;
+
+    urls_out.close()
+# #############################################################################
+# End file generation
+# #############################################################################
+else:
+    with open("urls.txt") as f:
+        urls = f.readlines()
+
+exercises = [];
+
+for url in urls:
+    data = {}
+    page = browser.open(url)
+
+    content = BeautifulSoup(page.read(), "lxml")
+    details = content.find(id="exerciseDetails")
+    guideContent = content.find("div", {"class" : "guideContent"})
+
+    name = details.h1.string.replace("\n","").strip()
+
+    newExercise = Exercise(name)
+
+    guide_items = [];
+    guide_notes = [];
+
+    for guide_item in guideContent.find_all("li"):
+        guide_items.append(guide_item.string)
+        # print guide_item.string
+
+    newExercise.set_guide_items(guide_items)
+    # data['guide_items'] = guide_items
+
+    for guide_note in guideContent.find_all("p"):
+        if not guide_note.strong:
+            note_title = guide_note.strong
+            # print note_title
+        guide_notes.append(guide_note.string)
+        # print guide_note.string
+
+    newExercise.set_note_title(note_title)
+    newExercise.set_notes(guide_notes)
+
+    # pp.pprint(guide_items);
+    # pp.pprint(guide_notes);
+
+    # data['Exercise Name'] = name
+
+    for span in details.find_all("span"):
+        key = span.getText().split(':')[0]
+
+        if "Login" in key:
+            break
+
+        value = span.getText().split(':')[1].replace("\n","").strip()
+
+        # setters
+        # if not mechanics type then must be general type
+        if "Mechanics" in key:
+            newExercise.set_mechanics(value)
+        elif "Type" in key:
+            newExercise.set_type(value)
+
+        if "Equipment" in key:
+            newExercise.set_equipment(value)
+        if "Force" in key:
+            newExercise.set_force(value)
+        if "Level" in key:
+            newExercise.set_level(value)
+        if "Main" in key:
+            newExercise.set_muscle(value)
+        if "Other" in key:
+            newExercise.set_other_muscles(value)
+        if "Sport" in key:
+            newExercise.set_sport(value)
+
+    exercises.append(newExercise)
